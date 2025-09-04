5fa4a29fa18b3542db5e7f23bc0746df584b3f24
michalhajok <65401178+michalhajok@users.noreply.github.com>
2025-09-04 20:07:14 +0200

    first

---
+++ src/app/(auth)/reset-password/layout.js
@@ -0,0 +1,15 @@
+import React, { Suspense } from "react";
+import Link from "next/link";
+import { ToastProvider } from "@/components/ui/Toast";
+
+export const metadata = {
+  title: "Uwierzytelnianie - FizjoCare",
+  description: "Zaloguj się do systemu zarządzania gabinetem fizjoterapii",
+  robots: "noindex, nofollow",
+};
+
+const AuthLayout = ({ children }) => {
+  return <Suspense>{children}</Suspense>;
+};
+
+export default AuthLayout;
